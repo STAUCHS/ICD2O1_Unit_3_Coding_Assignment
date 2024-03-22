116390849115fe03bb504a7ae24f60fbef874db1
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-22 13:53:27 -0400

    Updated

--- FahrenheitToCelsius.py
+++ FahrenheitToCelsius.py
@@ -5,17 +5,3 @@
 # Created:      22/03/2024
 #-------------------------------------------------------------------------
 
-# Output title
-print("** Fahrenheit to Celsius Converter **")
-
-# Get temperature in fahrenheit from user
-temp_fahrenheit = int(input("Enter the temperature in Fahrenheit: "))
-
-# Calculate celsius = (5/9)*(F-32)
-temp_celsius = (5/9) * (temp_fahrenheit - 32)
-temp_celsius = round(temp_celsius)
-temp_celsius = int(temp_celsius)
-
-# Output celsius
-print("")
-print(temp_fahrenheit, "degrees Fahrenheit is", temp_celsius, "degrees Celsius.")
